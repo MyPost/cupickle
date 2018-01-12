b8a812d705d2119da8dc948d10afd094fa4155b7
Alex Whitt <alex.joseph.whitt@gmail.com>
2018-01-12 15:15:12 -0500

    Update to 1.9.0

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/MyPost/cupickle"
   :license {:name "Apache License v2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [bultitude "0.2.6"]
                  [com.cemerick/pomegranate "0.3.0"]])
